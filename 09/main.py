060c42ce668eeab605f89b22c6d89b7a20e75086
Kevin <kevin.m.smiley@gmail.com>
2019-12-17 13:51:32 -0500

    use intcomputer module for 09

--- 09/main.py
+++ 09/main.py
@@ -1,5 +1,5 @@
 import intcomputer
 
 program = intcomputer.load("input")
-print(intcomputer.execute(program, [1]))
-print(intcomputer.execute(program, [2]))
+print(intcomputer.Computer(program, [1]).tick_until_output())
+print(intcomputer.Computer(program, [2]).tick_until_output())
